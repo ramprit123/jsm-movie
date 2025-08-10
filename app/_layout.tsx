d3d3a504baa74210243212d732e1ae46e51b16c5
ramprit123 <ramprit.sahani@outlook.com>
2025-08-10 17:37:00 +0530

    Add StatusBar component to RootLayout for improved UI consistency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import Provider from "@/components/Provider";
 import "@/styles/global.css";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="movie/[id]" />
       </Stack>
+      <StatusBar barStyle="light-content" />
     </Provider>
   );
 }
